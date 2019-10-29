f17e0e211a359da31dc96ef7c77e55f7d7fae4a1
Michael Caveney <dylanesque27@gmail.com>
2019-10-29 18:14:04 -0400

    Removed rating prototype

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require('jquery')
+require("jquery")
 
 import '../../assets/stylesheets/application.scss'
 import './bootstrap_custom.js'
